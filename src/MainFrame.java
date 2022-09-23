de9f1ac78e89af973b00b36e8f924f41c5a43547
Neo Argatides <neo.argatides@gmail.com>
2022-09-23 15:36:43 -0700

    button

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,10 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
         setTitle("Card Shuffle");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1400, 800);
+
+        JButton btnShuffle = new JButton("Shuffle");
+        btnShuffle.addActionListener(new ActionListener() { //redraw bars button
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(btnShuffle);
+        getContentPane().add(BorderLayout.SOUTH, btnPanel);
     }
 
     public static void main(String[] args) {
