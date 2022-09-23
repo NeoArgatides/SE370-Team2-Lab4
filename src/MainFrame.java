7f0e437c1c003d5bc34b70df30374b368f686ec4
Neo Argatides <neo.argatides@gmail.com>
2022-09-23 16:01:01 -0700

    huh

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,20 +1,30 @@
 import javax.swing.*;
-import java.util.Random;
-
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
+    ImageIcon cards[] = new ImageIcon[52];
+    
     public MainFrame() {
         setTitle("Card Shuffle");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1400, 800);
+
+        JButton btnShuffle = new JButton("Shuffle");
+        btnShuffle.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(btnShuffle);
+        getContentPane().add(BorderLayout.SOUTH, btnPanel);
     }
 
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
-
-
-        ImageIcon playingCards = new ImageIcon("PlayingCards/2_of_clubs.png");
-        //JLabel.setIcon(playingCards);
     }
 }
