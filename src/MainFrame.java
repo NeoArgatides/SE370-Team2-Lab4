ff52961ff71ab2455c7f40fe4a40f753243b019b
AbrahamRosales0311 <70557011+AbrahamRosales0311@users.noreply.github.com>
2022-09-23 15:54:38 -0700

    playing cards added to folder

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,28 +1,20 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Random;
+
 
 public class MainFrame extends JFrame {
     public MainFrame() {
         setTitle("Card Shuffle");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1400, 800);
-
-        JButton btnShuffle = new JButton("Shuffle");
-        btnShuffle.addActionListener(new ActionListener() { //redraw bars button
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        JPanel btnPanel = new JPanel();
-        btnPanel.add(btnShuffle);
-        getContentPane().add(BorderLayout.SOUTH, btnPanel);
     }
 
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
+
+
+        ImageIcon playingCards = new ImageIcon("PlayingCards/2_of_clubs.png");
+        JLabel.setIcon(playingCards);
     }
 }
